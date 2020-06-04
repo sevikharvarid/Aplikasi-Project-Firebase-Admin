8eab2c5a09e6035338334eabac39beab7c6dd8d7
sevikharvarid <sevikhar@gmail.com>
2020-06-04 22:25:12 +0700

    first commit

---
+++ app/src/main/java/com/example/faq_chatapp_admin/FeedBack3.java
@@ -0,0 +1,16 @@
+package com.example.faq_chatapp_admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.faq_chatapp_admin.R;
+
+public class FeedBack3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_back3);
+    }
+}
---
+++ app/src/main/java/com/example/faq_chatapp_admin/user.java
@@ -0,0 +1,43 @@
+package com.example.faq_chatapp_admin;
+
+public class user {
+
+    public String Name,Email,Phone;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public user()
+    {
+
+    }
+
+    public user(String Name,String Email,String Phone )
+    {
+        this.Name=Name;
+        this.Email=Email;
+        this.Phone=Phone;
+
+    }
+}
